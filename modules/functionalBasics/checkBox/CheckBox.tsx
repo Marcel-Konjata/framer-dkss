e6f5800b2e12e5e35ff44a6fac14d9f4056a5f37
marcel <marcel.konjata@carvago.com>
2021-05-30 23:17:22 +0200

    animated checkbox

--- modules/functionalBasics/checkBox/CheckBox.tsx
+++ modules/functionalBasics/checkBox/CheckBox.tsx
@@ -10,9 +10,18 @@ export const CheckBox: FC = () => {
 	const [isChecked, setIsChecked] = useState(false);
 
 	return (
-		<div
+		<motion.div
 			onClick={() => setIsChecked((s) => !s)}
 			className={styles.container}
+			// u can animate inline even conditionally based on state
+			animate={{ backgroundColor: isChecked ? checkedColor : whiteColor }}
+			// transition property modify animation with similar props as css counter part, however it can be injected directly as object key in animate property,
+			// very useful with complex orchestrations and variants, second way is used down below for this example
+			transition={{
+				duration: 0.3,
+				ease: "easeInOut",
+				delay: isChecked ? 0 : 0.4,
+			}}
 		>
 			<svg
 				width="28"
@@ -21,12 +30,18 @@ export const CheckBox: FC = () => {
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
 			>
-				<path
+				<motion.path
 					d="M2 12.4706C5.46988 18.8235 10.6747 27.4706 12.8434 31L32 1"
 					stroke="white"
 					strokeWidth="3"
+					// pathLength is special animatable property of svg motion path basically does what is name suggests
+					initial={{ pathLength: isChecked ? 1 : 0 }}
+					animate={{
+						pathLength: isChecked ? 1 : 0,
+						transition: { duration: 0.4, delay: isChecked ? 0.3 : 0 },
+					}}
 				/>
 			</svg>
-		</div>
+		</motion.div>
 	);
 };
