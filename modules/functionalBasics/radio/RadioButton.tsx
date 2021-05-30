1d5428c595e32182d8a6757cbe558f66ab73ccf4
marcel <marcel.konjata@carvago.com>
2021-05-30 23:43:56 +0200

    chained animation on radio button

--- modules/functionalBasics/radio/RadioButton.tsx
+++ modules/functionalBasics/radio/RadioButton.tsx
@@ -1,6 +1,33 @@
-import { motion, Variant, Variants } from "framer-motion";
+import { AnimationControls, motion, Variant, Variants } from "framer-motion";
 import React, { FC } from "react";
 import styles from "./RadioButton.module.scss";
+
+const shrinkVariant = (isActive: boolean): Variant => ({
+	scale: isActive ? 0.6 : 1,
+	x: isActive ? "-4px" : 0,
+	y: isActive ? "-4px" : 0,
+	transition: {
+		delay: isActive ? 0.3 : 0,
+	},
+});
+
+const pathVariant = (isActive: boolean): Variants => ({
+	stroke: {
+		pathLength: isActive ? 1 : 0,
+		transition: {
+			delay: !isActive ? 0.25 : 0,
+			duration: 0.25,
+		},
+	},
+
+	fill: {
+		fill: isActive ? "rgb(255, 255, 255)" : "rgba(0, 0, 0, 0)",
+		transition: {
+			delay: isActive ? 0.25 : 0,
+			duration: 0.2,
+		},
+	},
+});
 
 interface RadioButtonProps {
 	radioId: string;
@@ -15,34 +42,6 @@ export const RadioButton: FC<RadioButtonProps> = ({
 	groupName,
 	setIsActive,
 }) => {
-
-	const pathVariant: Variants = {
-		stroke: {
-			pathLength: isActive ? 1 : 0,
-			transition: {
-				delay: !isActive ? 0.25 : 0,
-				duration: 0.25,
-			},
-		},
-
-		fill: {
-			fill: isActive ? "rgb(255, 255, 255)" : "rgba(0, 0, 0, 0)",
-			transition: {
-				delay: isActive ? 0.25 : 0,
-				duration: 0.2,
-			},
-		},
-	};
-
-	const shrinkVariant: Variant = {
-		scale: isActive ? 0.6 : 1,
-		x: isActive ? "-4px" : 0,
-		y: isActive ? "-4px" : 0,
-		transition: {
-			delay: isActive ? 0.3 : 0,
-		},
-	};
-
 	return (
 		<>
 			<input
@@ -57,7 +56,7 @@ export const RadioButton: FC<RadioButtonProps> = ({
 				onClick={() => setIsActive(radioId)}
 			>
 				<motion.svg
-					animate={shrinkVariant}
+					animate={shrinkVariant(isActive) as AnimationControls}
 					className={styles.svg}
 					viewBox="0 0 41 41"
 					fill="none"
@@ -67,7 +66,8 @@ export const RadioButton: FC<RadioButtonProps> = ({
 						d="M40.5 20.5C40.5 31.5457 31.5457 40.5 20.5 40.5C9.4543 40.5 0.5 31.5457 0.5 20.5C0.5 9.4543 9.4543 0.5 20.5 0.5C31.5457 0.5 40.5 9.4543 40.5 20.5Z"
 						strokeWidth="3"
 						stroke="white"
-						variants={pathVariant}
+						variants={pathVariant(isActive)}
+						// this is not in docs: if you want to chain multiple animation on each other after the first in order stops, pass it as array of variants in order that u want them to play
 						animate={["stroke", "fill"]}
 					/>
 				</motion.svg>
