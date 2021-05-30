5cd262921b145ea1cc6911ac6008c43bdc39d575
marcel <marcel.konjata@carvago.com>
2021-05-30 22:52:47 +0200

    moving-underline

--- modules/homepage/sorter/Sorter.component.tsx
+++ modules/homepage/sorter/Sorter.component.tsx
@@ -1,14 +1,16 @@
 import React, { Dispatch, FC, SetStateAction } from "react";
 import styles from "./Sorter.module.scss";
-import {AnimateSharedLayout, motion} from "framer-motion";
+import { AnimateSharedLayout, motion } from "framer-motion";
 
 interface SorterProps {
-  activeSort: "asc" | "desc" | "shuffle";
-  setActiveSort: Dispatch<SetStateAction<"asc" | "desc" | "shuffle">>;
+	activeSort: "asc" | "desc" | "shuffle";
+	setActiveSort: Dispatch<SetStateAction<"asc" | "desc" | "shuffle">>;
 }
 
 export const Sorter: FC<SorterProps> = ({ activeSort, setActiveSort }) => {
 	return (
+		//animate shared layout implements algorithm for ALL motion elements with layoutID with same value
+		//independently even when they leaves or enters DOM
 		<AnimateSharedLayout>
 			<div className="mt-5 flex justify-center">
 				<div className="mr-10">
@@ -16,27 +18,28 @@ export const Sorter: FC<SorterProps> = ({ activeSort, setActiveSort }) => {
 						className="focus:outline-none"
 						onClick={() => setActiveSort("asc")}
 					>
-          ascendent sort
+						ascendent sort
 					</button>
-					{activeSort === "asc" && <div className={styles.underline} />}
+					{/*all of this elements are tagged with same layout id this will cause visual animations, the value of id is whatever u like, but must be same*/}
+					{activeSort === "asc" && <motion.div layout layoutId={"underline"} className={styles.underline} />}
 				</div>
 				<div>
 					<button
 						className="focus:outline-none"
 						onClick={() => setActiveSort("desc")}
 					>
-          descendent sort
+						descendent sort
 					</button>
-					{activeSort === "desc" && <div className={styles.underline} />}
+					{activeSort === "desc" && <motion.div layout layoutId={"underline"} className={styles.underline} />}
 				</div>
 				<div className="ml-10">
 					<button
 						className="focus:outline-none"
 						onClick={() => setActiveSort("shuffle")}
 					>
-          shuffle
+						shuffle
 					</button>
-					{activeSort === "shuffle" && <div className={styles.underline} />}
+					{activeSort === "shuffle" && <motion.div layout layoutId={"underline"} className={styles.underline} />}
 				</div>
 			</div>
 		</AnimateSharedLayout>
